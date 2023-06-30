24a47ca6d18fcc7d0f3eb8b22037754b3246ef97
Barry <barryarr3@gmail.com>
2023-06-30 14:20:08 +0300

    Изменён файл task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+
+  let discriminant = b ** 2 - 4 * a * c;
+  
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1, x2);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+}
+
+"use strict";
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (typeof percent !== 'number' || typeof contribution !== 'number' || typeof amount !== 'number' || typeof countMonths !== 'number') {
+    return false;
+  } else {
+    let monthlyPercent = percent / 100 / 12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (monthlyPercent + monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
+    let totalAmount = monthlyPayment * countMonths;
+    return Number(totalAmount.toFixed(2));
+  }
 }
